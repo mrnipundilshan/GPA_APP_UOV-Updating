2118a770feb34335be9203cb31aa164d3bebfca2
nipun <nipundilshan991@gmail.com>
2024-05-14 16:49:17 +0530

    newchanges

---
+++ lib/fianresult.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:newgpa/firstsecond.dart';
+
+class finalrseult extends StatefulWidget {
+  const finalrseult({Key? key}) : super(key: key);
+
+  @override
+  State<finalrseult> createState() => _finalrseultState();
+}
+
+class _finalrseultState extends State<finalrseult> {
+  // Create separate instances of Dropdownservice
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: const Color.fromARGB(255, 94, 167, 88),
+          centerTitle: true,
+          title: const Text(
+            "1ST YEAR 1ST SEMESTER",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(13),
+          width: 420,
+          decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 38, 46, 37),
+              image: DecorationImage(
+                  image: AssetImage('images/bg1.png'), opacity: 0.08)),
+          alignment: Alignment.topCenter,
+          child: ListView(
+            //scrollDirection: Axis.vertical,
+            children: [],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Color.fromARGB(255, 75, 150, 68),
+          height: 55,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          firstsecond(),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        return child;
+                      },
+                    ),
+                  );
+                },
+                child: Text('Next Sem'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
